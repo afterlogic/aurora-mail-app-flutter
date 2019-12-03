ab77841e0f52af4fa8502e4c62426725c5d28f6a
Vadim <vadeseg@gmail.com>
2019-12-03 13:28:11 +0300

    implemented sending token as query param for message view images

--- lib/modules/mail/screens/message_view/components/message_body.dart
+++ lib/modules/mail/screens/message_view/components/message_body.dart
@@ -3,12 +3,10 @@ import 'dart:convert';
 import 'package:aurora_mail/database/app_database.dart';
 import 'package:aurora_mail/modules/auth/blocs/auth_bloc/bloc.dart';
 import 'package:aurora_mail/modules/mail/models/mail_attachment.dart';
-import 'package:aurora_mail/utils/api_utils.dart';
 import 'package:aurora_mail/utils/mail_utils.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -53,39 +51,41 @@ class _MessageBodyState extends State<MessageBody> {
       return null;
     }
 
-    setState(() => _htmlData = htmlData);
+//    setState(() => _htmlData = htmlData);
 
     for (final attachment in widget.attachments) {
-      try {
-        final res = await http.get(
-            AuthBloc.currentUser.hostname + attachment.viewUrl,
-            headers: getHeaderWithToken());
-        if (!mounted) break;
+//      try {
+//        final res = await http.get(
+//            AuthBloc.currentUser.hostname + attachment.viewUrl,
+//            headers: getHeaderWithToken());
+//        if (!mounted) break;
+//
+//        htmlData = htmlData.replaceFirst(
+//          "data-x-src-cid=\"${attachment.cid}\"",
+//          "src=\"data:image/png;base64, ${base64Encode(res.bodyBytes)}\"",
+////          "data:image/png;base64," + base64Encode(res.bodyBytes),
+//        );
+//        _controller?.loadUrl(_getHtmlUri(htmlData));
+//
+//        setState(() => _htmlData = htmlData);
+//      } catch (err, s) {
+//        print("err: ${err}");
+//        print("s: ${s}");
+//      }
 
-        htmlData = htmlData.replaceFirst(
-          "data-x-src-cid=\"${attachment.cid}\"",
-          "src=\"data:image/png;base64, ${base64Encode(res.bodyBytes)}\"",
-//          "data:image/png;base64," + base64Encode(res.bodyBytes),
-        );
-        _controller?.loadUrl(_getHtmlUri(htmlData));
-
-        setState(() => _htmlData = htmlData);
-      } catch (err, s) {
-        print("err: ${err}");
-        print("s: ${s}");
-      }
-
-//      htmlData = htmlData.replaceFirst(
-//        "cid:${attachment.cid}",
-//        AuthBloc.currentUser.hostname + attachment.viewUrl,
-//      );
+      htmlData = htmlData.replaceFirst(
+        "data-x-src-cid=\"${attachment.cid}\"",
+        "src=\"${AuthBloc.currentUser.hostname}${attachment.viewUrl.replaceFirst("mail-attachment/", "mail-attachments-cookieless/")}&AuthToken=${AuthBloc.currentUser.token}\"",
+      );
     }
+
+    setState(() => _htmlData = htmlData);
   }
 
   String _getHtmlUri(String html) {
     final wrappedHtml = MailUtils.wrapInHtml(context, html);
     return Uri.dataFromString(wrappedHtml,
-        mimeType: 'text/html', encoding: Encoding.getByName('utf-8'))
+            mimeType: 'text/html', encoding: Encoding.getByName('utf-8'))
         .toString();
   }
 
@@ -103,7 +103,8 @@ class _MessageBodyState extends State<MessageBody> {
             child: AnimatedOpacity(
               opacity: _pageLoaded && _htmlData != null ? 0.0 : 1.0,
               duration: Duration(milliseconds: 200),
-              child: Container(color: Theme.of(context).scaffoldBackgroundColor),
+              child:
+                  Container(color: Theme.of(context).scaffoldBackgroundColor),
 //            child: Container(color: Colors.red),
             ),
           ),
