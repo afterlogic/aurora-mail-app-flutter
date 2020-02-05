af85e08261cdcc58bdbe54eaf552b49f45e7a5cc
Vadim <vadeseg@gmail.com>
2020-02-05 11:34:07 +0300

    fixed logo and added afterlogic_custom build

---
+++ build_res/afterlogic_custom/theme/lib/app_color.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+class AppColor {
+  static final primary = Color(0xFF75B8F3);
+  static final primaryVariant = Color(0xFF1A7396);
+
+  static final secondary = Color(0xFF095365);
+  static final secondaryVariant = Color(0x99095365);
+
+  static final accent = Color(0xFFE55046);
+  static final accentVariant = Color(0xFFE55046);
+
+  static final surface = Color(0xFFF9EEBF);
+
+  static final warning = Color(0xFFF44336);
+}
