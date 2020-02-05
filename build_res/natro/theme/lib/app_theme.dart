08c23ed767c42cced4cce44fb6927b9444a69f70
Vadim <vadeseg@gmail.com>
2020-02-05 16:00:27 +0300

    natro build variant fixes

--- build_res/natro/theme/lib/app_theme.dart
+++ build_res/natro/theme/lib/app_theme.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 class AppTheme {
   static final theme = ThemeData(
     brightness: Brightness.light,
-    primarySwatch: Colors.red,
+    primarySwatch: Colors.blue
   );
 
   static final darkTheme = ThemeData(
     brightness: Brightness.dark,
-    primarySwatch: Colors.red,
+      primarySwatch: Colors.blue
   );
 }
