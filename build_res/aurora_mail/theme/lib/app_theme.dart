9dbd13c0f457905a30abe76b75a994cb878f7605
Vadim <vadeseg@gmail.com>
2020-02-10 15:09:30 +0300

    added aurora_mail build variant

---
+++ build_res/aurora_mail/theme/lib/app_theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final theme = ThemeData(
+    brightness: Brightness.light,
+    primarySwatch: Colors.blue
+  );
+
+  static final darkTheme = ThemeData(
+    brightness: Brightness.dark,
+      primarySwatch: Colors.blue
+  );
+}
