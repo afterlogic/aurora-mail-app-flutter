4b61414c9abbeba22e1fcfa7ea9f79a5aecf36cc
Nikita Yakovlev <princesakenny98@gmail.com>
2020-03-05 19:40:33 +0300

    fix ios app name

--- ios/Runner/ValidSignVerify.swift
+++ ios/Runner/ValidSignVerify.swift
@@ -10,23 +10,23 @@ import BouncyCastle_ObjC
 import DMSOpenPGP
 
 public class ValidDMSPGPClearTextVerifier {
-
+    
     public let cleartext: String
     public let signature: String
-
+    
     public let hashHeaders: [String]
     public let message: String
     public let signatureList: BCOpenpgpPGPSignatureList?
-
+    
     public init(cleartext: String) throws {
         self.cleartext = cleartext
-
+        
         let scanner = Scanner(string: cleartext)
         scanner.charactersToBeSkipped = nil
-
+        
         // Jump to cleartext signature begin
         scanner.scanUpTo("-----BEGIN PGP SIGNED MESSAGE-----", into: nil)
-
+        
         // Read -----BEGIN PGP SIGNED MESSAGE-----\r\n
         var signedMessageHeader: NSString?
         scanner.scanUpToCharacters(from: .newlines, into: &signedMessageHeader)
@@ -34,12 +34,12 @@ public class ValidDMSPGPClearTextVerifier {
         guard signedMessageHeader == "-----BEGIN PGP SIGNED MESSAGE-----" else {
             throw DMSPGPError.invalidCleartext
         }
-
+        
         // Read armor headers
         var hashHeaders: [String] = []
         var nextLine: NSString? = ""
         var lastScanLocation: Int
-
+        
         repeat {
             lastScanLocation = scanner.scanLocation
             scanner.scanUpToCharacters(from: .newlines, into: &nextLine)
@@ -49,75 +49,79 @@ public class ValidDMSPGPClearTextVerifier {
             }
             nextLine = nil
             hashHeaders.append(hashHeader as String)
-
+            
             if !scanner.scanUpToCharacters(from: .newlines, into: &nextLine) {
                 // got one empty line
                 // no more hash header
                 break
             }
-
+            
             if lastScanLocation == scanner.scanLocation {
                 // scanner not move
                 throw DMSPGPError.invalidCleartext
             }
         } while lastScanLocation != scanner.scanLocation
         self.hashHeaders = hashHeaders
-
+        
         // Read one empty line
         scanner.scanString("\n", into: nil)
-
+        
         // Read cleartext
         var rawMessage: NSString?
         scanner.scanUpTo("-----BEGIN PGP SIGNATURE-----", into: &rawMessage)
         let message = rawMessage as String?     // Message could be empty
         
-        let end = message!.index(message!.startIndex, offsetBy: message!.count-2)
-        self.message = String(message![...end])
-           
+        if(message!.count<=2){
+            self.message = ""
+        }else{
+            let end =  message!.index(message!.startIndex, offsetBy: message!.count-2)
+            self.message = String(message![...end])
+        }
+        
         // Read footer
         var footer: NSString?
         scanner.scanUpTo("-----END PGP SIGNATURE-----", into: &footer)
-
+        
         guard var signature = (footer as String?), !signature.isEmpty else {
             throw DMSPGPError.invalidCleartext
         }
         signature.append(contentsOf: "-----END PGP SIGNATURE-----")
         self.signature = signature
-
+        
         self.signatureList = try! DMSPGPClearTextVerifier.signatureList(from: signature)
     }
-
+    
 }
 extension ValidDMSPGPClearTextVerifier {
-
+    
     public var signatureVerifier: DMSPGPSignatureVerifier {
         return DMSPGPSignatureVerifier(message: message, onePassSignatureList: nil, signatureList: signatureList)
     }
-
+    
 }
 
 extension DMSPGPSignatureVerifier {
-
+    
     public func verifySignature(message:String, use publicKeyRing: BCOpenpgpPGPPublicKeyRing) -> VerifyResult {
         guard let signatureList = signatureList, !signatureList.isEmpty(),
-        !signatureListKeyInfos.isEmpty else {
-            return .noSignature
+            !signatureListKeyInfos.isEmpty else {
+                return .noSignature
         }
-
+        
         guard let signatureKey = publicKeyRing.primarySignatureKey else {
             return VerifyResult.unknownSigner(signatureListKeyInfos)
         }
-
+        
         guard let iterator = signatureList.iterator() else {
             return .noSignature
         }
-
+        
         while iterator.hasNext() {
             guard let signature = iterator.next() as? BCOpenpgpPGPSignature,
-            signatureKey.keyID == signature.keyID else {
-                continue
+                signatureKey.keyID == signature.keyID else {
+                    continue
             }
-
+            
             let builderProvider = BCOpenpgpOperatorJcajceJcaPGPContentVerifierBuilderProvider()
                 .setProviderWith(BCJceProviderBouncyCastleProvider.PROVIDER_NAME)
             signature.init__(with: builderProvider, with: signatureKey)
@@ -126,27 +130,27 @@ extension DMSPGPSignatureVerifier {
                 return .valid
             }
         }
-
+        
         return .invalid
     }
-
+    
 }
 
 public class ValidDMSPGPDecryptor {
-
+    
     public let armoredMessage: String
     public let encryptingKeyIDs: [String]
-
+    
     public let encryptedDataDict: [String: BCOpenpgpPGPPublicKeyEncryptedData]
     public let hiddenRecipientsDataList: [BCOpenpgpPGPPublicKeyEncryptedData]
-
+    
     private(set) public var onePassSignatureList: BCOpenpgpPGPOnePassSignatureList?
     private(set) public var signatureList: BCOpenpgpPGPSignatureList?
     private(set) public var modificationTime: Date?
-
+    
     public init(armoredMessage message: String) throws {
         self.armoredMessage = message
-
+        
         let byteArray = IOSByteArray(nsData: Data(message.utf8))!
         let input = JavaIoByteArrayInputStream(byteArray: byteArray)
         guard let armoredInput = BCOpenpgpPGPUtil.getDecoderStream(with: input) as? BCBcpgArmoredInputStream else {
@@ -156,35 +160,35 @@ public class ValidDMSPGPDecryptor {
             input.close()
             armoredInput.close()
         }
-
+        
         // Get encrypted data list
         var encryptedDataList: BCOpenpgpPGPEncryptedDataList?
         do {
             let result = try ExceptionCatcher.catchException {
                 let objectFactory = BCOpenpgpPGPObjectFactory(javaIoInputStream: armoredInput, with: BCOpenpgpOperatorJcajceJcaKeyFingerprintCalculator())
-
+                
                 var object = objectFactory.nextObject()
                 while object != nil {
                     guard let list = object as? BCOpenpgpPGPEncryptedDataList else {
                         object = objectFactory.nextObject()
                         continue
                     }
-
+                    
                     return list
                 }
-
+                
                 return nil
             }
-
+            
             encryptedDataList = result as? BCOpenpgpPGPEncryptedDataList
         } catch {
             // continue decrypt if got encryptedDataList
         }
-
+        
         guard let iterator = encryptedDataList?.iterator() else {
             throw DMSPGPError.invalidMessage
         }
-
+        
         // Get encrypted data
         var keyIDs = Set<String>()
         var encryptedDataDict: [String: BCOpenpgpPGPPublicKeyEncryptedData] = [:]
@@ -193,7 +197,7 @@ public class ValidDMSPGPDecryptor {
             guard let data = iterator.next() as? BCOpenpgpPGPPublicKeyEncryptedData else {
                 continue
             }
-
+            
             let keyID = String(fromPGPKeyID: data.getKeyID())
             if keyID.isHiddenRecipientID {
                 hiddenRecipientsDataList.append(data)
@@ -202,25 +206,25 @@ public class ValidDMSPGPDecryptor {
                 encryptedDataDict[keyID] = data
             }
         }
-
+        
         guard (!keyIDs.isEmpty && !encryptedDataDict.isEmpty) || !hiddenRecipientsDataList.isEmpty else {
             throw DMSPGPError.invalidMessage
         }
-
+        
         self.encryptingKeyIDs = Array(keyIDs)
         self.encryptedDataDict = encryptedDataDict
         self.hiddenRecipientsDataList = hiddenRecipientsDataList
     }
-
+    
 }
 
 extension ValidDMSPGPDecryptor {
-
+    
     public func decrypt(secretKey: BCOpenpgpPGPSecretKey, password: String) throws -> String {
         guard let privateKey = secretKey.getEncryptingPrivateKey(password: password) else {
             throw DMSPGPError.invalidSecrectKeyPassword
         }
-
+        
         return try decrypt(privateKey: privateKey, keyID: secretKey.keyID)
     }
     
@@ -295,7 +299,7 @@ extension ValidDMSPGPDecryptor {
     }
 }
 extension ValidDMSPGPDecryptor {
-
+    
     /// Verify armored message
     ///
     /// - Parameter message: armored message
@@ -304,16 +308,16 @@ extension ValidDMSPGPDecryptor {
         guard let byteArray = IOSByteArray(nsData: Data(message.utf8)) else {
             return false
         }
-
+        
         let input = JavaIoByteArrayInputStream(byteArray: byteArray)
         let inputStream: BCBcpgArmoredInputStream? = try! ExceptionCatcher.catchException {
             return BCOpenpgpPGPUtil.getDecoderStream(with: input)
-        } as? BCBcpgArmoredInputStream
-
+            } as? BCBcpgArmoredInputStream
+        
         guard inputStream != nil else {
             return false
         }
-
+        
         return true
     }
 }
