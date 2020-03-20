747e6f3acdde21d61861040ada14032b643902cd
Nikita Yakovlev <princesakenny98@gmail.com>
2020-03-20 15:54:09 +0300

    build_all_all

--- sh/build_all_all.sh
+++ sh/build_all_all.sh
@@ -1,8 +1,6 @@
 
-./sh/build_variant.sh $1/afterlogic/build_variant.yaml
-./sh/build_apk.sh
-./sh/build_appbundle.sh
+./sh/build_variant.sh build_res/afterlogic/build_variant.yaml
+./sh/build_all.sh
 
-./sh/build_variant.sh $1/private_mail/build_variant.yaml
-./sh/build_apk.sh
-./sh/build_appbundle.sh
+./sh/build_variant.sh build_res/private_mail/build_variant.yaml
+./sh/build_all.sh
