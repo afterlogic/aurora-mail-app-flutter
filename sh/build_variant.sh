241573f7edadf5d0bd6120ce4523e13b7368def1
Nikita Yakovlev <princesakenny98@gmail.com>
2020-07-30 14:13:52 +0300

    added warning after update customer info

--- sh/build_variant.sh
+++ sh/build_variant.sh
@@ -3,3 +3,10 @@ flutter pub get
 flutter pub run build_variant $1
 flutter pub get
 flutter pub run flutter_launcher_icons:main
+echo "--------------------------------------"
+echo ""
+echo ""
+echo "       check xcode permissions!"
+echo ""
+echo ""
+echo "--------------------------------------"
