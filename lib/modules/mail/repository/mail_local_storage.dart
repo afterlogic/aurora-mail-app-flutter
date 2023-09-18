471c5f5f944b2a6c167f10ee6d8c15f6c1a1a700
HtonicSava <htonic.sava@gmail.com>
2023-09-18 16:15:29 +0300

    unnecessary file permission requests are removed

--- lib/modules/mail/repository/mail_local_storage.dart
+++ lib/modules/mail/repository/mail_local_storage.dart
@@ -5,7 +5,6 @@ import 'package:file_picker/file_picker.dart';
 
 class MailLocalStorage {
   Future<File> pickFile({FileType type = FileType.any}) async {
-    await getStoragePermissions();
     return FilePicker.platform
         .pickFiles(type: type ?? FileType.any)
         .then((value) => value != null ? File(value.files.first.path) : null);
