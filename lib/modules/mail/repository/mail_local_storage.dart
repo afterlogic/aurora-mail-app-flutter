00ae19b7c0d389dee27535fed12eebbecfe1d7bf
Vasil Sokolov <vasil@afterlogic.com>
2023-12-07 10:12:42 +0200

    Fixed files attaching issue

--- lib/modules/mail/repository/mail_local_storage.dart
+++ lib/modules/mail/repository/mail_local_storage.dart
@@ -5,6 +5,8 @@ import 'package:file_picker/file_picker.dart';
 
 class MailLocalStorage {
   Future<File> pickFile({FileType type = FileType.any}) async {
+    await getStoragePermissions();
+    FilePicker.platform.clearTemporaryFiles();
     return FilePicker.platform
         .pickFiles(type: type ?? FileType.any)
         .then((value) => value != null ? File(value.files.first.path) : null);
