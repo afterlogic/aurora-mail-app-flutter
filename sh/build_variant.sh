c8794c7d84742d045ac047060bef39570e4069a6
Vasil Sokolov <vasil.sokolov@gmail.com>
2024-04-16 15:46:44 +0200

    Updated build-variant.sh script

--- sh/build_variant.sh
+++ sh/build_variant.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-flutter pub get
-flutter pub run build_variant $1
-flutter pub get
-flutter pub run flutter_launcher_icons:main
+RED='\033[1;31m'
+YELLOW='\033[1;33m'
+GREEN='\033[1;32m'
+NC='\033[0m' # No Color
+
+if [ -f "$1" ]; then
+    flutter pub get
+    flutter pub run build_variant $1
+    flutter pub get
+    flutter pub run flutter_launcher_icons:main
+else
+    printf $RED"No build_variant.yaml file is found. Abborting.\n"$NC
+fi
