7e988ee58df69ea48ede3b7d7189d9696470819f
HtonicSava <htonic.sava@gmail.com>
2024-04-16 17:03:54 +0300

    deletion selected contacts feature

--- lib/modules/contacts/screens/contacts_list/components/select_app_bar.dart
+++ lib/modules/contacts/screens/contacts_list/components/select_app_bar.dart
@@ -1,8 +1,10 @@
+import 'package:aurora_mail/generated/l10n.dart';
 import 'package:aurora_mail/modules/contacts/blocs/contacts_bloc/contacts_bloc.dart';
 import 'package:aurora_mail/modules/contacts/blocs/contacts_bloc/events/contacts_event.dart';
 import 'package:aurora_mail/modules/contacts/contacts_domain/models/contact_model.dart';
 import 'package:aurora_mail/modules/contacts/screens/components/groups_select_dialog.dart';
 import 'package:aurora_mail/modules/mail/screens/messages_list/components/selection_controller.dart';
+import 'package:aurora_mail/shared_ui/confirmation_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
@@ -28,8 +30,28 @@ class SelectAppBar extends StatelessWidget{
       actions: [IconButton(
         icon: Icon(MdiIcons.fileMove),
         onPressed: () => _moveToGroup(context),
+      ), IconButton(
+        icon: Icon(Icons.delete_outline),
+        onPressed: () => _deleteContacts(context),
       )],
     );
+  }
+
+  void _deleteContacts(BuildContext context) async {
+    final contacts = controller.selected.entries.map((e) => e.value).toList();
+    final delete = await ConfirmationDialog.show(
+      context,
+      S.of(context).contacts_delete_title_plural,
+      S.of(context).contacts_delete_selected,
+      S.of(context).btn_delete,
+      destructibleAction: true,
+    );
+    if (delete != true) {
+      return;
+    }
+
+    bloc.add(DeleteContacts(contacts));
+    controller.enable = false;
   }
 
 
