3063f186f4de532d62534216ada11d3d6776b5fb
HtonicSava <htonic.sava@gmail.com>
2024-10-07 10:20:52 +0300

    paddings for tasks update

--- lib/modules/calendar/ui/views/tasks_view.dart
+++ lib/modules/calendar/ui/views/tasks_view.dart
@@ -48,56 +48,53 @@ class _TasksViewState extends State<TasksView> {
                 : SizedBox.shrink();
           },
         ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                child: Row(
-                  children: [
-                    const Text(
-                      'Tasks',
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
-                    ),
-                    const Spacer(),
-                    BlocBuilder<TasksBloc, TasksState>(
-                      bloc: _bloc,
-                      builder: (context, state) {
-                        return IconButton(
-                          padding: EdgeInsets.zero,
-                          constraints: BoxConstraints(),
-                          icon: Icon(Icons.filter_alt_outlined),
-                          onPressed: () {
-                            ActivityFilterDialog.show(context,
-                                    selectedFilter: state.filter)
-                                .then((value) {
-                              if (value == null) return;
-                              _bloc.add(UpdateFilter(value));
-                            });
-                          },
-                        );
-                      },
-                    )
-                  ],
-                ),
+        Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 24),
+              child: Row(
+                children: [
+                  const Text(
+                    'Tasks',
+                    style:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+                  ),
+                  const Spacer(),
+                  BlocBuilder<TasksBloc, TasksState>(
+                    bloc: _bloc,
+                    builder: (context, state) {
+                      return IconButton(
+                        padding: EdgeInsets.zero,
+                        constraints: BoxConstraints(),
+                        icon: Icon(Icons.filter_alt_outlined),
+                        onPressed: () {
+                          ActivityFilterDialog.show(context,
+                                  selectedFilter: state.filter)
+                              .then((value) {
+                            if (value == null) return;
+                            _bloc.add(UpdateFilter(value));
+                          });
+                        },
+                      );
+                    },
+                  )
+                ],
               ),
-              Expanded(
-                child: BlocBuilder<TasksBloc, TasksState>(
-                  bloc: _bloc,
-                  builder: (context, state) {
-                    if (state.tasks == null) {
-                      return const SizedBox.shrink();
-                    }
-                    return _TaskList(
-                      tasks: state.tasks!,
-                    );
-                  },
-                ),
+            ),
+            Expanded(
+              child: BlocBuilder<TasksBloc, TasksState>(
+                bloc: _bloc,
+                builder: (context, state) {
+                  if (state.tasks == null) {
+                    return const SizedBox.shrink();
+                  }
+                  return _TaskList(
+                    tasks: state.tasks!,
+                  );
+                },
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ],
     );
@@ -115,7 +112,7 @@ class _TaskList extends StatelessWidget {
       itemCount: tasks.length,
       itemBuilder: (context, index) {
         return Padding(
-          padding: const EdgeInsets.symmetric(vertical: 12.0),
+          padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 24),
           child: TaskWidget(task: tasks[index]),
         );
       },
