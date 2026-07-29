e8d912bad3924060139617232354eb284fdcd918
Vasil Sokolov <vasil.sokolov@gmail.com>
2026-07-29 17:50:23 +0200

    Fixed incorrect thumbnail of atteched file on compose screen

--- lib/modules/mail/screens/compose/components/thumbnail_widget.dart
+++ lib/modules/mail/screens/compose/components/thumbnail_widget.dart
@@ -53,7 +53,12 @@ class _ThumbnailWidgetState extends State<ThumbnailWidget> {
         } else {}
       } catch (e) {}
     }
-    if (provider == null && widget.attachment?.thumbnailUrl != null) {
+    final mimeType = widget.attachment?.mimeType;
+    final isImageOrVideo = mimeType != null &&
+        (mimeType.startsWith("image/") || mimeType.startsWith("video/"));
+    if (provider == null &&
+        isImageOrVideo &&
+        widget.attachment?.thumbnailUrl != null) {
       final user = BlocProvider.of<AuthBloc>(context).currentUser!;
       provider = NetworkImage(
         user.hostname + widget.attachment!.thumbnailUrl!,
